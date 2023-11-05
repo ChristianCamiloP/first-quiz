12ce6c638c715229c5e1dbf05de12fab72325421
ChristianCamiloP <149966264+ChristianCamiloP@users.noreply.github.com>
2023-11-05 14:59:42 -0500

    Update VendingMachineImpl.java

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -10,8 +10,28 @@ public class VendingMachineImpl implements VendingMachine {
     public VendingMachineImpl() {
         this.currentMoney = 0;
         this.drinks = new HashMap<>();
-        drinks.put("ScottCola", new DrinkImpl("ScottCola", true));
-        drinks.put("KarenTea", new DrinkImpl("KarenTea", false));
+        drinks.put("ScottCola", new Drink() {
+            @Override
+            public String getName() {
+                return "ScottCola";
+            }
+
+            @Override
+            public boolean isFizzy() {
+                return true;
+            }
+        });
+        drinks.put("KarenTea", new Drink() {
+            @Override
+            public String getName() {
+                return "KarenTea";
+            }
+
+            @Override
+            public boolean isFizzy() {
+                return false;
+            }
+        });
     }
 
     public static VendingMachine getInstance() {
