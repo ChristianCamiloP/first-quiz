c585d84778a88fc4da1c31ddd31b48aa3618cf52
ChristianCamiloP <149966264+ChristianCamiloP@users.noreply.github.com>
2023-11-05 14:53:21 -0500

    Update VendingMachineImpl.java

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,40 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+import java.util.HashMap;
+import java.util.Map;
 
-  public static VendingMachine getInstance() {
-    // Fix me!
-    return null;
-  }
+public class VendingMachineImpl implements VendingMachine {
+    private int currentMoney;
+    private Map<String, Drink> drinks;
+
+    public VendingMachineImpl() {
+        this.currentMoney = 0;
+        this.drinks = new HashMap<>();
+        drinks.put("ScottCola", new DrinkImpl("ScottCola", true));
+        drinks.put("KarenTea", new DrinkImpl("KarenTea", false));
+    }
+
+    public static VendingMachine getInstance() {
+        return new VendingMachineImpl();
+    }
+
+    @Override
+    public void insertQuarter() {
+        currentMoney += 25;
+    }
+
+    @Override
+    public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+        if (drinks.containsKey(name)) {
+            Drink drink = drinks.get(name);
+            if (currentMoney >= 75) {
+                currentMoney -= 75;
+                return drink;
+            } else {
+                throw new NotEnoughMoneyException();
+            }
+        } else {
+            throw new UnknownDrinkException();
+        }
+    }
 }
